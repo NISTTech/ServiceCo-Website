6941a3e825f9a1f3ee62a13a301ff659e574f7d3
SoloStudio <147620785+tpmptuvejp@users.noreply.github.com>
2024-08-25 20:00:50 +0700

    add logo wall

--- index.php
+++ index.php
@@ -23,6 +23,41 @@
                     <a href="./servicegroups/anglewishes/index.php"><img src="./servicegroups/anglewishes/logo.png" alt="anglewishes Logo"></a>
                     <a href="./servicegroups/arc/index.php"><img src="./servicegroups/arc/logo.png" alt="arc Logo"></a>
                     <a href="./servicegroups/baansaijai/index.php"><img src="./servicegroups/baansaijai/logo.png" alt="baansaijai Logo"></a>
+                    <a href="./servicegroups/beatasone/index.php"><img src="./servicegroups/beatasone/logo.png" alt="beatasone Logo"></a>
+                    <a href="./servicegroups/catnist/index.php"><img src="./servicegroups/catnist/logo.png" alt="catnist Logo"></a>
+                    <a href="./servicegroups/cof/index.php"><img src="./servicegroups/cof/logo.png" alt="cof Logo"></a>
+                    <a href="./servicegroups/connectingcommunities/index.php"><img src="./servicegroups/connectingcommunities/logo.png" alt="connectingcommunities Logo"></a>
+                    <a href="./servicegroups/cured/index.php"><img src="./servicegroups/cured/logo.png" alt="cured Logo"></a>
+                    <a href="./servicegroups/eco/index.php"><img src="./servicegroups/eco/logo.png" alt="eco Logo"></a>
+                    <a href="./servicegroups/edibubble/index.php"><img src="./servicegroups/edibubble/logo.png" alt="edibubble Logo"></a>
+                    <a href="./servicegroups/fairnist/index.php"><img src="./servicegroups/fairnist/logo.png" alt="fairnist Logo"></a>
+                    <a href="./servicegroups/farmersmarket/index.php"><img src="./servicegroups/farmersmarket/logo.png" alt="farmersmarket Logo"></a>
+                    <a href="./servicegroups/fashionist/index.php"><img src="./servicegroups/fashionist/logo.png" alt="fashionist Logo"></a>
+                    <a href="./servicegroups/feminist/index.php"><img src="./servicegroups/feminist/logo.png" alt="feminist Logo"></a>
+                    <a href="./servicegroups/forestrangers/index.php"><img src="./servicegroups/forestrangers/logo.png" alt="forestrangers Logo"></a>
+                    <a href="./servicegroups/freetobe/index.php"><img src="./servicegroups/freetobe/logo.png" alt="freetobe Logo"></a>
+                    <a href="./servicegroups/friendsoftheimmanuleorchestra/index.php"><img src="./servicegroups/friendsoftheimmanuleorchestra/logo.png" alt="friendsoftheimmanuleorchestra Logo"></a>
+                    <a href="./servicegroups/greenlight/index.php"><img src="./servicegroups/greenlight/logo.png" alt="greenlight Logo"></a>
+                    <a href="./servicegroups/helpinghearts/index.php"><img src="./servicegroups/helpinghearts/logo.png" alt="helpinghearts Logo"></a>
+                    <a href="./servicegroups/mushiemushie/index.php"><img src="./servicegroups/mushiemushie/logo.png" alt="mushiemushie Logo"></a>
+                    <a href="./servicegroups/nest/index.php"><img src="./servicegroups/nest/logo.png" alt="nest Logo"></a>
+                    <a href="./servicegroups/nistcha/index.php"><img src="./servicegroups/nistcha/logo.png" alt="nistcha Logo"></a>
+                    <a href="./servicegroups/nistcomunitymarket/index.php"><img src="./servicegroups/nistcomunitymarket/logo.png" alt="nistcomunitymarket Logo"></a>
+                    <a href="./servicegroups/nisttech/index.php"><img src="./servicegroups/nisttech/logo.png" alt="nisttech Logo"></a>
+                    <a href="./servicegroups/operationsmile/index.php"><img src="./servicegroups/operationsmile/logo.png" alt="operationsmile Logo"></a>
+                    <a href="./servicegroups/pfn/index.php"><img src="./servicegroups/pfn/logo.png" alt="pfn Logo"></a>
+                    <a href="./servicegroups/phuliphay/index.php"><img src="./servicegroups/phuliphay/logo.png" alt="phuliphay Logo"></a>
+                    <a href="./servicegroups/priceless/index.php"><img src="./servicegroups/priceless/logo.png" alt="priceless Logo"></a>
+                    <a href="./servicegroups/rescuedglass/index.php"><img src="./servicegroups/rescuedglass/logo.png" alt="rescuedglass Logo"></a>
+                    <a href="./servicegroups/revive/index.php"><img src="./servicegroups/revive/logo.png" alt="revive Logo"></a>
+                    <a href="./servicegroups/rooftopgarden/index.php"><img src="./servicegroups/rooftopgarden/logo.png" alt="rooftopgarden Logo"></a>
+                    <a href="./servicegroups/soap/index.php"><img src="./servicegroups/soap/logo.png" alt="soap Logo"></a>
+                    <a href="./servicegroups/stepup/index.php"><img src="./servicegroups/stepup/logo.png" alt="stepup Logo"></a>
+                    <a href="./servicegroups/thefalconer/index.php"><img src="./servicegroups/thefalconer/logo.png" alt="thefalconer Logo"></a>
+                    <a href="./servicegroups/tusk/index.php"><img src="./servicegroups/tusk/logo.png" alt="tusk Logo"></a>
+                    <a href="./servicegroups/wellnist/index.php"><img src="./servicegroups/wellnist/logo.png" alt="wellnist Logo"></a>
+                    <a href="./servicegroups/wishtogether/index.php"><img src="./servicegroups/wishtogether/logo.png" alt="wishtogether Logo"></a>
+                    <a href="./servicegroups/woof/index.php"><img src="./servicegroups/woof/logo.png" alt="woof Logo"></a>
                 </div>
             </section>
         </main>
