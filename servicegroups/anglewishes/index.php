9c1745d4c79fbb29ee3f22194e3b7a0277c5c0a9
SoloStudio <147620785+tpmptuvejp@users.noreply.github.com>
2024-09-01 19:21:18 +0700

    service group page test

---
+++ servicegroups/anglewishes/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="./logo.png">
+        <title>AngleWishes</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <header>
+            <a href="http://localhost/ServiceCo/servicegroups/anglewishes/index.php"><img src="./logo.png" alt="anglewishes Logo"></a>
+            <h1>Welcome to Angle Wishes</h1>
+            <a href="http://localhost/ServiceCo/index.php" class="home-button"> Back To ServiceCo</a>
+        </header>
+        <main>
+            <section id="goals">
+                <h2>Our Goals</h2>
+                <p>To raise awareness and promote learning on the realities of cancer and cancer treatment in the NIST community, in addition to providing social, emotional, and financial support to patients diagnosed with cancer and their loved ones, in partnership with Angel Foundation.</p>
+            </section>
+            <section id="products">
+                <h2>Our Products</h2>
+                <!--Place holder for products-->
+            </section>
+        </main>
+        <footer>
+            <p>&copy; 2024 AngleWishes</p>
+        </footer>
+    </body>
+</html>
