909d18618a2c5cdc977c04ffa0660eb53b716274
uqnquvwfkq <147620785+uqnquvwfkq@users.noreply.github.com>
2025-03-20 07:18:32 +0700

    Update search.php

--- search.php
+++ search.php
@@ -2,8 +2,9 @@
 $conn = new mysqli("localhost", "root", "", "serviceco");
 
 $searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
+
 $resultServiceGroup = $conn->query("SELECT * FROM service_groups WHERE name LIKE '%$searchTerm%'");
-$resultProducts = $conn->query("SELECT * FROM products WHERE description LIKE '%$searchTerm%'");
+$resultProducts = $conn->query("SELECT * FROM products WHERE name LIKE '%$searchTerm%' OR description LIKE '%$searchTerm%'");
 
 $results = [];
 
@@ -19,7 +20,7 @@ if ($resultServiceGroup->num_rows > 0) {
 if ($resultProducts->num_rows > 0) {
     $results[] = "<div class='results-header'>Products:</div>";
     while ($productRow = $resultProducts->fetch_assoc()) {
-        $results[] = "<div class='product-item'><a href='#'>" . $productRow['name'] . "</a></div>";
+        $results[] = "<div class='product-item'><a href='http://localhost/ServiceCo/servicegroups/p_template.php?productId=" . $productRow['id'] . "'>" . $productRow['name'] . "</a></div>";
     }
 } else {
     $results[] = "<div class='error'>No products found.</div>";
